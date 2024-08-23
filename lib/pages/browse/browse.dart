f0a23637c7dfecbcff73da522cf00273d64e5ea8
Ahmed <alizghloula@gmail.com>
2024-08-23 18:25:06 +0300

    Finish Search Screen.

--- lib/pages/browse/browse.dart
+++ lib/pages/browse/browse.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Browse extends StatelessWidget {
-
-
+  static const String routeName='browse';
   @override
   Widget build(BuildContext context) {
     return  Container(color: Colors.blue,);
--- lib/pages/watch_list/watch_list.dart
+++ lib/pages/watch_list/watch_list.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class WatchList extends StatelessWidget {
-
-
+  static const String routeName='watchList';
   @override
   Widget build(BuildContext context) {
     return  Container(color: Colors.green,);
