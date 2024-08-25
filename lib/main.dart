0b1d173917f5d2860a33b5ae16e8385ccedfedf6
EL-mohands <amrmarei87@gmail.com>
2024-08-25 11:13:53 -0700

    saving edit..

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:movie/my_theme.dart';
-import 'package:movie/pages/home_screen/home_screen.dart';
 import 'package:movie/pages/home_screen/my_bloc_observer.dart';
 import 'package:movie/pages/init_route/init_route.dart';
 
 void main (){
   Bloc.observer = MyBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseFirestore.instance.enableNetwork();
   runApp(MyApp());
 }
 
