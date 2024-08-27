c23c525f79702f09eff360060cfab80435b9dc29
ezzeldeen377 <ezzeldeentv20@gmail.com>
2024-08-28 01:09:46 +0300

    fix some errors

--- lib/pages/home_screen/home_screen.dart
+++ lib/pages/home_screen/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.blackColor,
       body: SingleChildScrollView(
-        child: Column(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               margin: EdgeInsets.symmetric(vertical: 8),
@@ -20,16 +20,23 @@ class HomeScreen extends StatelessWidget {
                   0.52, // النسبة التي يشغلها العنصر الأول
               child: PopularDetails(),
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('New Releases',style: TextStyle(color: AppColors.whiteColor,fontSize: 24),),
+            ),
             Container(
               height: MediaQuery.of(context).size.height *
-                  0.5, // النسبة التي يشغلها العنصر الثاني
+                  0.4, // النسبة التي يشغلها العنصر الثاني
               child: ReleasesDetailsView(),
             ),
-            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Recommended',style: TextStyle(color: AppColors.whiteColor,fontSize: 24),),
+            ),
             Container(
               margin: EdgeInsets.symmetric(vertical: 8),
               height: MediaQuery.of(context).size.height *
-                  0.5, // النسبة التي يشغلها العنصر الثالث
+                  0.4, // النسبة التي يشغلها العنصر الثالث
               child: RecommendeDetails(),
             ),
             SizedBox(height: 10),
