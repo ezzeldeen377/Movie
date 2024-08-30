5ab40d2cde1601d8732a8fb43b2ae5cc5950144f
ezzeldeen377 <ezzeldeentv20@gmail.com>
2024-08-30 21:48:52 +0300

    Finish all project with shared preferences on localization

--- lib/app_provider/app_provider.dart
+++ lib/app_provider/app_provider.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppProvider extends ChangeNotifier {
   String appLanguage;
 
   // Constructor with an optional initial language parameter
-  AppProvider({this.appLanguage = 'ar'});
+  AppProvider({this.appLanguage = 'en'}){
+    initProvider();
+  }
 
-  void changeLanguage(String newLanguage) {
-    if (newLanguage == appLanguage) {
+  Future<void>  initProvider() async {
+    try {
+
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      appLanguage = prefs.getString('language')!;
+      notifyListeners() ;
+
+    }catch(e){
+      print(e.toString());
+    }
+  }
+  void changeLanguage(String newLanguage)async {
+    if(appLanguage == newLanguage){
       return;
     }
-    appLanguage = newLanguage;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('language',newLanguage);
+    appLanguage=newLanguage;
     notifyListeners();
   }
 }
